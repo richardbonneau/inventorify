5d71548ef34d5a66e0cdf64dc2774cca210b253c
richardbonneau <richardbonneau@hotmail.com>
2018-09-12 15:04:53 -0400

    changed package json for heroku. new scripts

--- client/components/Main.js
+++ client/components/Main.js
@@ -62,6 +62,7 @@ export default class Main extends Component {
                             })
                     })
                 }
+
                 //console.log("products in list: ", this.state.selectedProductsByUser)
             }
         })
